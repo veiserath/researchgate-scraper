95b47ad4b7606f00c1f94c2155a0f6a10789f575
Kacper Mlodkowski <kacpermlodkowski@Kacpers-MacBook-Pro.local>
2022-03-15 17:16:40 +0100

    Added export to json and citations

--- main.py
+++ main.py
@@ -1,23 +1,66 @@
+import time
 from bs4 import BeautifulSoup
 import requests
 import json
 from lxml import etree
 
-url = "https://www.researchgate.net/publication/346429437_Optimizing_clinical_research_procedures_in_public_health_emergencies"
+url = "https://www.researchgate.net/publication/220591356_Identifying_Software_Project_Risks_An_International_Delphi_Study"
 request = requests.get(url)
 parser = "html.parser"
 soup = BeautifulSoup(request.text, parser)
+dom = etree.HTML(str(soup))
+
+
+def get_count(string):
+    count = ""
+    for item in string:
+        if item.isdigit():
+            count += item
+    return count
+
+
+
+
+
+def get_reference_article_citation_count(url):
+    time.sleep(2)
+    request = requests.get(url)
+    parser = "html.parser"
+    soup = BeautifulSoup(request.text, parser)
+    dom = etree.HTML(str(soup))
+    citation = str(dom.xpath("//div[contains(text(),'Citations')]/text()")[0])
+    return get_count(citation)
 
 
 def get_citation_count():
-    dom = etree.HTML(str(soup))
     citation = str(dom.xpath("//div[contains(text(),'Citations')]/text()")[0])
-    # references = str(dom.xpath("//div[contains(text(),'References')]/text()")[0])
-    return citation
+    return get_count(citation)
+
+
+def get_references_count():
+    references = str(dom.xpath("//div[contains(text(),'References')]/text()")[0])
+    return get_count(references)
 
 
 def get_ld_json() -> dict:
     return json.loads("".join(soup.find("script", {"type": "application/ld+json"}).contents))
+
+
+def export_to_pdf():
+    paper_info = {
+        'title': result["headline"],
+        'url': result["mainEntityOfPage"],
+        'date': result["datePublished"],
+        'publisher': result["publisher"]["name"],
+        'citation count': get_citation_count(),
+        'reference count': get_references_count(),
+        'references': get_references(),
+        'citations': get_citations()
+    }
+    result_json = json.dumps(paper_info)
+
+    with open("result.json", "w") as outfile:
+        outfile.write(result_json)
 
 
 def print_main_article_metadata():
@@ -29,6 +72,36 @@ def print_main_article_metadata():
         print(item["name"])
     print(result["publisher"]["name"])
     print("====")
+
+
+def get_references():
+    references = []
+    i = 0
+    for item in result["citation"]:
+        if i >= citations_range:
+            break
+        paper_info = {
+            'title': item["headline"],
+            'url': item["mainEntityOfPage"],
+            'date': item["datePublished"],
+            'publisher': item["publisher"]["name"],
+        }
+        references.append(paper_info)
+        i += 1
+    return references
+
+
+def get_citations():
+    citations = []
+    for i in range(citations_range, len(result["citation"])):
+        paper_info = {
+            'title': result["citation"][i]["headline"],
+            'url': result["citation"][i]["mainEntityOfPage"],
+            'date': result["citation"][i]["datePublished"],
+            'publisher': result["citation"][i]["publisher"]["name"],
+        }
+        citations.append(paper_info)
+    return citations
 
 
 def print_references():
@@ -46,7 +119,15 @@ def print_references():
         print("===")
 
 
+
+
+
 get_citation_count()
 result = get_ld_json()
-print_main_article_metadata()
-print_references()
+
+
+
+citations_range = len(result["citation"]) - 1 - len(get_citation_count())
+# print_main_article_metadata()
+# print_references()
+export_to_pdf()
