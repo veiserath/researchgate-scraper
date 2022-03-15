98f61041921fc72c52ef127694f2619b4f8410a5
Kacper Mlodkowski <kacpermlodkowski@Kacpers-MacBook-Pro.local>
2022-03-15 12:21:56 +0100

    Added citation count

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-import json
 from bs4 import BeautifulSoup
 import requests
+import json
+from lxml import etree
+
+url = "https://www.researchgate.net/publication/346429437_Optimizing_clinical_research_procedures_in_public_health_emergencies"
+request = requests.get(url)
+parser = "html.parser"
+soup = BeautifulSoup(request.text, parser)
 
 
-url = "https://www.researchgate.net/publication/324010445_Changing_the_Model_of_Maritime_Navigation"
+def get_citation_count():
+    dom = etree.HTML(str(soup))
+    citation = str(dom.xpath("//div[contains(text(),'Citations')]/text()")[0])
+    # references = str(dom.xpath("//div[contains(text(),'References')]/text()")[0])
+    return citation
 
 
-def get_ld_json(url: str) -> dict:
-    parser = "html.parser"
-    req = requests.get(url)
-    soup = BeautifulSoup(req.text, parser)
-    # return json.loads("".join(soup.find('script', {'type': 'application/ld+json'})))
-    p = str(soup.find('script', {'type': 'application/ld+json'}))
-    return json.loads("".join(soup.find("script", {"type":"application/ld+json"}).contents))
+def get_ld_json() -> dict:
+    return json.loads("".join(soup.find("script", {"type": "application/ld+json"}).contents))
 
-def print_main_article_metadata(json):
+
+def print_main_article_metadata():
     print(result["headline"])
     print(result["datePublished"])
     print(result["mainEntityOfPage"])
@@ -24,17 +30,23 @@ def print_main_article_metadata(json):
     print(result["publisher"]["name"])
     print("====")
 
-result = get_ld_json(url)
-print_main_article_metadata(json)
+
+def print_references():
+    i = 0
+    for item in result["citation"]:
+        print(i)
+        print(item["headline"])
+        print(item["datePublished"])
+        print(item["mainEntityOfPage"])
+        # make it a one-liner
+        for subitem in item["author"]:
+            print(subitem["name"])
+        print(item["publisher"]["name"])
+        i += 1
+        print("===")
 
 
-
-for item in result["citation"]:
-    print(item["headline"])
-    print(item["datePublished"])
-    print(item["mainEntityOfPage"])
-    # make it a one-liner
-    for subitem in item["author"]:
-        print(subitem["name"])
-    print(item["publisher"]["name"])
-    print("===")
+get_citation_count()
+result = get_ld_json()
+print_main_article_metadata()
+print_references()
